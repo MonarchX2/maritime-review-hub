a883fabb78e62bf6f645d209e5655d7d38ff5b11
MonarchX2 <aeroblancaflor@gmail.com>
2026-07-11 19:41:13 +0800

    ggg

--- sw.js
+++ sw.js
@@ -3,9 +3,7 @@ const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
+    'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', (event) => {
