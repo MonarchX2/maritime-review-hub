39485a0a38167c206559dc6b71174239dbeda6b6
MonarchX2 <aeroblancaflor@gmail.com>
2026-07-15 09:37:56 +0800

    dfgfdg

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mrh-v16'; // Bumped version to force update
+const CACHE_NAME = 'mrh-v17'; // Bumped version to force update
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
